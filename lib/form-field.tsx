5fd34b37a0f06d218305f4870f1f18c63b164861
fffirer <0sdf0@sina.cn>
2024-11-22 11:57:29 +0800

    update form-field disabled&readonly

--- lib/form-field.tsx
+++ lib/form-field.tsx
@@ -1,37 +1,62 @@
-import { createMemo, splitProps, useContext, type ComponentProps, type ValidComponent } from "solid-js";
-import type { FormFieldProps, ValueAccessibleComponent, ValueAccessor } from "./types";
+import {
+  createMemo,
+  splitProps,
+  useContext,
+  type ComponentProps,
+  type ValidComponent,
+} from "solid-js";
+import type {
+  FormFieldProps,
+  ValueAccessibleComponent,
+  ValueAccessor,
+} from "./types";
 import { FieldContextProvider, useFormContext } from "./contexts";
 import FormControl from "./form-control";
 import utils from "./utils";
 
-const FormField = <V, C extends ValueAccessibleComponent<V, ValueAccessor<V>> | ValidComponent, P extends ComponentProps<C>, K extends keyof P>(props: FormFieldProps<V, C, P, K>) => {
-  const [local, field] = splitProps(props, ['control', 'controlProps', 'controlValuePropName', 'onControlValueChanged', 'children'])
+const FormField = <
+  V,
+  C extends ValueAccessibleComponent<V, ValueAccessor<V>> | ValidComponent,
+  P extends ComponentProps<C>,
+  K extends keyof P
+>(
+  props: FormFieldProps<V, C, P, K>
+) => {
+  const [local, field] = splitProps(props, [
+    "control",
+    "controlProps",
+    "controlValuePropName",
+    "onControlValueChanged",
+    "children",
+  ]);
   const form = useFormContext();
 
   const controlProps = createMemo(() => {
-    const ctrlProps: Record<string, any> = {}
+    const ctrlProps: Record<string, any> = {};
 
     if (utils.IsValidFieldName(field.name)) {
-      ctrlProps['name'] = field.name
+      ctrlProps["name"] = field.name;
     }
 
     return {
       ...ctrlProps,
       ...local.controlProps,
-      disabled: form?.disabled(),
-      readonly: form?.readonly()
-    } as any
-  })
+      disabled: form?.disabled() ?? local.controlProps?.disabled,
+      readonly: form?.readonly() ?? local.controlProps?.readonly,
+    } as any;
+  });
 
-  return <FieldContextProvider {...field}>
-    <FormControl
-      control={local.control}
-      controlProps={controlProps()}
-      controlValuePropName={local.controlValuePropName}
-      onControlValueChanged={local.onControlValueChanged}>
-    </FormControl>
-    {local.children}
-  </FieldContextProvider>
-}
+  return (
+    <FieldContextProvider {...field}>
+      <FormControl
+        control={local.control}
+        controlProps={controlProps()}
+        controlValuePropName={local.controlValuePropName}
+        onControlValueChanged={local.onControlValueChanged}
+      ></FormControl>
+      {local.children}
+    </FieldContextProvider>
+  );
+};
 
-export default FormField
+export default FormField;
