db8bb26ca0789bb0e498fc83af60071bf48e8895
fffirer <0sdf0@sina.cn>
2025-04-19 23:56:51 +0800

    fix

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,12 +1,26 @@
 import { type Component, type JSX, splitProps } from "solid-js";
 import type { ValueAccessor } from "../../lib";
 
-const Input: Component<ValueAccessor<any> & Omit<JSX.InputHTMLAttributes<HTMLInputElement>, 'value' | 'onchange' | 'onChange'>> = (props) => {
-    const [local, elProps] = splitProps(props, ['value', 'onValueChanged'])
-    const handleChanged: JSX.InputHTMLAttributes<HTMLInputElement>['onChange'] = e => {
-        local.onValueChanged?.(e.target.value)
-    }
-    return <input {...props} value={local.value} onChange={handleChanged}></input>
-}
+const Input: Component<
+  ValueAccessor<any> &
+    Omit<
+      JSX.InputHTMLAttributes<HTMLInputElement>,
+      "value" | "onchange" | "onChange"
+    >
+> = (props) => {
+  const [local, elProps] = splitProps(props, ["value", "onValueChanged"]);
+  const handleChanged: JSX.InputHTMLAttributes<HTMLInputElement>["onChange"] = (
+    e
+  ) => {
+    local.onValueChanged?.(e.target.value);
+  };
+  return (
+    <input
+      {...props}
+      value={local.value ?? null}
+      onChange={handleChanged}
+    ></input>
+  );
+};
 
-export default Input
+export default Input;
