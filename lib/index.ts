b5da6ddd843c96a27da4764757d659eb92a633a6
fffirer <0sdf0@sina.cn>
2025-04-29 14:12:54 +0800

    update

--- lib/index.ts
+++ lib/index.ts
@@ -3,6 +3,8 @@ export { default as FormField } from './form-field'
 export { default as FormControl } from './form-control'
 export { default as Form } from './form'
 export { default as utils } from './utils'
+export { default as FormList } from './form-list'
+export * from './form-list'
 export * from './core'
 
 export * from './types'
